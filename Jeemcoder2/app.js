10dc1478c82492aba49982104e95f0396c02a85f
Mariama Balde <foulah18@gmail.com>
2024-08-30 21:34:50 +0000

    commit jeemacoder2

---
+++ Jeemcoder2/app.js
@@ -0,0 +1,107 @@
+class Jeemacoder extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            prenomInput: "",
+            nomInput: "",
+            emailInput: "",
+            telephoneInput: "",
+            coders: []
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick() {
+        const newCoder = {
+            prenom: this.state.prenomInput,
+            nom: this.state.nomInput,
+            email: this.state.emailInput,
+            telephone: this.state.telephoneInput
+        }
+        this.setState({ coders: [newCoder, ...this.state.coders] })
+        this.state({
+            prenomInput: "",
+            nomInput: "",
+            emailInput: "",
+            telephoneInput: "",
+        })
+    }
+
+    render() {
+        return (
+            <div className="container col-lg-6 m-auto col-12">
+                <h5 className="text-center mt-2">Jeemacoder</h5>
+                <div className="">
+                    <div className="row">
+                        <div class="col-lg-6 py-2">
+                            <label className="form-label">Prénom</label>
+                            <input type="text"
+                                value={this.state.prenomInput}
+                                onChange={(e) => {
+                                    this.setState({ prenomInput: e.target.value })
+                                }}
+                                className="form-control" />
+                        </div>
+                        <div class="col-lg-6 py-2">
+                            <label className="form-label">Nom</label>
+                            <input type="text"
+                                value={this.state.nomInput}
+                                onChange={(e) => {
+                                    this.setState({ nomInput: e.target.value })
+                                }}
+                                className="form-control" />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div class="col-lg-6 py-2">
+                            <label className="form-label">Email</label>
+                            <input type="email"
+                                value={this.state.emailInput}
+                                onChange={(e) => {
+                                    this.setState({ emailInput: e.target.value })
+                                }}
+                                className="form-control" />
+                        </div>
+                        <div class="col-lg-6 py-2">
+                            <label className="form-label">Telephone {this.state.telephoneInput}</label>
+                            <input type="number"
+                                value={this.state.telephoneInput}
+                                onChange={(e) => {
+                                    this.setState({ telephoneInput: e.target.value })
+                                }}
+                                className="form-control" />
+                        </div>
+                    </div>
+                    <button onClick={this.handleClick} class="btn btn-success w-100">Submit</button>
+                </div>
+                <div className="mt-5 container">
+                    <h3 className="text-center">Coders</h3>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Prénom</th>
+                                <th scope="col">Nom</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Telephone</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                          {
+                            this.state.coders.map((coder)=>{
+                                return <tr>
+                                    <td>{coder.prenom}</td>
+                                    <td>{coder.nom}</td>
+                                    <td>{coder.email}</td>
+                                    <td>{coder.telephone}</td>
+                                </tr>  
+                            })
+                          }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Jeemacoder />, document.getElementById("root"))
