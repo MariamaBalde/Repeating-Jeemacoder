4545aeb03724394baabdc6851d9b41a9cde8c537
Mariama Balde <foulah18@gmail.com>
2024-09-04 21:01:25 +0000

    adding btn modif

--- Jeemcoder3/app.js
+++ Jeemcoder3/app.js
@@ -9,6 +9,7 @@ class Jeemacoder extends React.Component {
             coders:[]
         }
         this.handleClick=this.handleClick.bind(this)
+        this.handleEdit=this.handleEdit.bind(this)
     }
 
     handleClick(){
@@ -25,6 +26,16 @@ class Jeemacoder extends React.Component {
             emailInput:"",
             telelphoneInput:""
         })
+    }
+
+    handleEdit(index){
+const coder=this.state.coders[index]
+this.setState({
+    prenomInput:coder.prenom,
+    nomInput:coder.nom,
+    emailInput:coder.email,
+    telelphoneInput:coder.telephone
+})
     }
 
     render() {
@@ -83,16 +94,20 @@ class Jeemacoder extends React.Component {
                                 <th scope="col">Nom</th>
                                 <th scope="col">Email</th>
                                 <th scope="col">Telephone</th>
+                                <th scope>Actions</th>
                             </tr>
                         </thead>
                         <tbody>
                        {
-                        this.state.coders.map((coder)=>{
+                        this.state.coders.map((coder,index)=>{
                             return <tr>
                                 <td>{coder.prenom}</td>
                                 <td>{coder.nom}</td>
                                 <td>{coder.email}</td>
                                 <td>{coder.telephone}</td>
+                                <td>
+                                    <button onClick={()=>this.handleEdit(index)} className="btn btn-warning">Modifier</button>
+                                    </td>
                             </tr>
                         })
                        }
