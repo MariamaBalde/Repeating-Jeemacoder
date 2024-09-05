15af8f91a32abc9f2121c4d6dc70c5511a347349
Mariama Balde <foulah18@gmail.com>
2024-09-05 21:30:00 +0000

    add btn modif

--- Jeemcoder5/app.js
+++ Jeemcoder5/app.js
@@ -1,30 +1,41 @@
 class Jeemacoder extends React.Component {
     constructor(props) {
         super(props)
-           this.state={
-            prenomInput:"",
-            nomInput:"",
-            emailInput:"",
-            telephoneInput:"",
-            coders:[]
-           }
+        this.state = {
+            prenomInput: "",
+            nomInput: "",
+            emailInput: "",
+            telephoneInput: "",
+            coders: []
+        }
         this.handleClick = this.handleClick.bind(this)
+        this.handleEdit=this.handleEdit.bind(this)
     }
 
-    handleClick(){
-        const  newCoder={
-            prenom:this.state.prenomInput,
-            nom:this.state.nomInput,
-            email:this.state.emailInput,
-            telephone:this.state.telephoneInput
+    handleClick() {
+        const newCoder = {
+            prenom: this.state.prenomInput,
+            nom: this.state.nomInput,
+            email: this.state.emailInput,
+            telephone: this.state.telephoneInput
         }
 
-        this.setState({coders:[newCoder,...this.state.coders]})
+        this.setState({ coders: [newCoder, ...this.state.coders] })
         this.setState({
-            prenomInput:"",
-            nomInput:"",
-            emailInput:"",
-            telephoneInput:"",
+            prenomInput: "",
+            nomInput: "",
+            emailInput: "",
+            telephoneInput: "",
+        })
+    }
+
+    handleEdit(index){
+        const coder=this.state.coders[index]
+        this.setState({
+            prenomInput:coder.prenom,
+            nomInput:coder.nom,
+            emailInput:coder.email,
+            telephoneInput:coder.telephone
         })
     }
 
@@ -37,19 +48,19 @@ class Jeemacoder extends React.Component {
                         <div class="col-lg-6 py-2">
                             <label class="form-label">Prenom</label>
                             <input type="text"
-                            value={this.state.prenomInput}
-                            onChange={(e)=>{
-                                this.setState({prenomInput:e.target.value})
-                            }}
+                                value={this.state.prenomInput}
+                                onChange={(e) => {
+                                    this.setState({ prenomInput: e.target.value })
+                                }}
                                 class="form-control" />
                         </div>
                         <div class="col-lg-6 py-2">
                             <label class="form-label">Nom</label>
                             <input type="text"
-                            value={this.state.nomInput}
-                            onChange={(e)=>{
-                                this.setState({nomInput:e.target.value})
-                            }}
+                                value={this.state.nomInput}
+                                onChange={(e) => {
+                                    this.setState({ nomInput: e.target.value })
+                                }}
                                 class="form-control" />
                         </div>
                     </div>
@@ -57,19 +68,19 @@ class Jeemacoder extends React.Component {
                         <div class="col-lg-6 py-2">
                             <label class="form-label">Email</label>
                             <input type="email"
-                            value={this.state.emailInput}
-                            onChange={(e)=>{
-                                this.setState({emailInput:e.target.value})
-                            }}
+                                value={this.state.emailInput}
+                                onChange={(e) => {
+                                    this.setState({ emailInput: e.target.value })
+                                }}
                                 class="form-control" />
                         </div>
                         <div class="col-lg-6 py-2">
                             <label class="form-label">Telephone</label>
                             <input type="number"
-                            value={this.state.telephoneInput}
-                            onChange={(e)=>{
-                                this.setState({telephoneInput:e.target.value})
-                            }}
+                                value={this.state.telephoneInput}
+                                onChange={(e) => {
+                                    this.setState({ telephoneInput: e.target.value })
+                                }}
                                 class="form-control" />
                         </div>
                     </div>
@@ -84,19 +95,23 @@ class Jeemacoder extends React.Component {
                                 <th scope="col">Nom</th>
                                 <th scope="col">Email</th>
                                 <th scope="col">Telephone</th>
+                                <th>Actions</th>
                             </tr>
                         </thead>
                         <tbody>
-                      {
-                        this.state.coders.map((coder)=>{
-                            return <tr>
-                                <td>{coder.prenom}</td>
-                                <td>{coder.nom}</td>
-                                <td>{coder.email}</td>
-                                <td>{coder.telephone}</td>
-                            </tr>
-                        })
-                      }
+                            {
+                                this.state.coders.map((coder,index) => {
+                                    return <tr>
+                                        <td>{coder.prenom}</td>
+                                        <td>{coder.nom}</td>
+                                        <td>{coder.email}</td>
+                                        <td>{coder.telephone}</td>
+                                        <td>
+                                            <button onClick={()=>this.handleEdit(index)} className="btn btn-warning">Modifier</button>
+                                        </td>
+                                    </tr>
+                                })
+                            }
                         </tbody>
                     </table>
                 </div>
